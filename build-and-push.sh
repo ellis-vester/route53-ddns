97f59d8ef7d75fd68a27ee161a34a4124cd01c19
Ellis Vester <ellis_vester@protonmail.com>
2021-04-02 00:47:15 -0400

    Removed unneeded build script

--- build-and-push.sh
+++
@@ -1,6 +0,0 @@
-$AWS_ACCOUNT_ID
-$AWS_REGION
-
-docker build -t route53-ddns .
-docker tag route53-ddns $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/route53-ddns
-docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/route53-ddns
